2c8ffe332dc7dda04efd4bbdfd9073ce43c4b609
ivolo <ivolo@mit.edu>
2014-05-25 16:55:39 +1000

    initial commit

---
+++ Readme.md
@@ -0,0 +1,77 @@
+
+# metrics-stripe-subscriptions
+
+A [Stripe](https://stripe.com) [subscriptions](https://github.com/stripe-subscriptions) plugin for [segmentio/metrics](https://github.com/segmentio/metrics).
+
+Use this plugin to visualize Stripe subscriptions over time.
+
+![](https://f.cloud.github.com/assets/658544/2361169/09325510-a62e-11e3-8f49-e327e89595cd.png)
+
+## Installation
+
+    $ npm install metrics-stripe-subscriptions
+
+## Quickstart
+
+Here's a full example of a [Geckoboard](https://github.com/segmentio/geckoboard) dashboard showing Stripe subscription metrics:
+
+```js
+var Metrics = require('metrics');
+var subscriptions = require('metrics-stripe-subscriptions');
+var geckoboard = require('geckoboard')('api-key');
+
+new Metrics()
+  .every('10m', subscriptions('stripe-key'))
+  .use(function (metrics) {
+    metrics.on('stripe subscriptions today', geckboard('widget-id').number);
+  });
+```
+
+#### Filter Customers
+
+You can further `filter` customers using [stripe-subscriptions](https://github.com/segmentio/stripe-chages) filters:
+
+```js
+new Metrics()
+  .every('10m', subscriptions('stripe-key', { filter: filter }))
+
+function filter (customer) {
+  return customer.id !== 'cus_8239d2jd9j'; // filter enterprise customer X
+}
+```
+
+## Metrics
+
+The metrics exposed by this plugin are divided by date granularity.
+
+Daily:
+- `stripe subscriptions today` - the number of subscriptions today
+- `stripe subscription mrr today` - the amount subscription mrr today
+- `stripe subscriptions yesterday` - the number of subscriptions yesterday
+- `stripe subscription mrr yesterday` - the amount subscription mrr yesterday
+- `stripe subscriptions 2 days ago` - the number of subscriptions 2 days ago
+- `stripe subscription mrr 2 days ago` - the amount subscription mrr 2 days ago
+
+Weekly:
+- `stripe subscriptions past week` - the number of subscriptions last week
+- `stripe subscription mrr past week` - total subscription mrr last week
+- `stripe subscriptions 2 weeks ago` - the number of subscriptions 2 weeks ago
+- `stripe subscription mrr 2 weeks ago` - total subscription mrr 2 weeks ago
+
+Monthly:
+- `stripe subscriptions past month` - the number of subscriptions last month
+- `stripe subscription mrr past month` - total subscription mrr last month
+- `stripe subscriptions 2 months ago` - the number of subscriptions 2 months ago
+- `stripe subscription mrr 2 months ago` - total subscription mrr 2 months ago
+
+Total: 
+- `stripe subscriptions` - total amount of Stripe subscriptions
+- `stripe subscription mrr` - total amount subscription mrr
+
+Weekly Sparkline: 
+- `stripe subscriptions last week` - an array of subscriptions in the past 7 days
+- `stripe subscription mrr last week` - an array of subscription amounts in the past 7 days
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,115 @@
+
+var debug = require('debug')('metrics:stripe:subscriptions');
+var Cohort = require('stripe-cohort');
+var Dates = require('date-math');
+
+/**
+ * Expose `subscriptions`.
+ */
+
+module.exports = subscriptions;
+
+/**
+ * Return a Stripe subscriptions plugin.
+ * @param {String} key
+ * @param {Object} options
+ */
+
+function subscriptions (key, options) {
+  var query = Cohort(key);
+  options = options || {};
+  // default filter function
+  var filter =  options.filter || function () { return true; };
+  var plans =  options.plans || {};
+  return function (metrics) {
+    // select all the subscriptions
+    debug('querying stripe subscriptions ..');
+    query(new Date(0), new Date(), function (err, customers) {
+      if (err) return debug('failed to get stripe subscriptions: %s', err);
+
+      var subscriptions = customers.filter(filter).subscriptions()
+        .paid()
+        .active();
+
+      debug('found %d stripe subscriptions', subscriptions.count());
+
+      // by time
+      var today = new Date();
+      set(metrics, subscriptions, plans, 'all time', new Date(0), today);
+      set(metrics, subscriptions, plans, 'today', floor(today), today);
+      set(metrics, subscriptions, plans, 'yesterday', Dates.day.shift(today, -1), today);
+      set(metrics, subscriptions, plans, '2 days ago', Dates.day.shift(today, -2), today);
+      set(metrics, subscriptions, plans, 'last week', Dates.week.shift(today, -1), today);
+      set(metrics, subscriptions, plans, '2 weeks ago', Dates.week.shift(today, -2), Dates.week.shift(today, -1));
+      set(metrics, subscriptions, plans, 'last month', Dates.month.shift(today, -1), today);
+      set(metrics, subscriptions, plans, '2 months ago', Dates.month.shift(today, -2), Dates.month.shift(today, -1));
+
+      // daily
+      daily(metrics, subscriptions);
+    });
+  };
+}
+
+/**
+ * Set metrics for the given `key` and time interval.
+ *
+ * @param {Metrics} metrics
+ * @param {Array|Charge} subscriptions
+ * @param {String} key
+ * @param {Object} plans
+ * @param {Date} start
+ * @param {Date} end
+ */
+
+function set (metrics, subscriptions, plans, key, start, end) {
+  subscriptions = subscriptions.started(start, end);
+  metrics.set('stripe subscriptions ' + key, subscriptions.count());
+  metrics.set('stripe subscriptions mrr ' + key, subscriptions.mrr());
+
+  Object.keys(plans).forEach(function (name) {
+    var id = plans[name];
+    var s = subscriptions.plan(id);
+    metrics.set('stripe ' + name + ' subscriptions ' + key, s.count());
+    metrics.set('stripe ' + name + ' subscriptions mrr ' + key, s.mrr());
+  });
+}
+
+/**
+ * Get the daily subscription counts for the last week
+ *
+ * @param {Array|Metric} metrics
+ * @param {Array|Charge} subscriptions
+ */
+
+function daily (metrics, subscriptions) {
+  var today = new Date();
+
+  var numbers = [];
+  var amounts = [];
+
+  for (var ago = 7; ago >= 0; ago -= 1) {
+    var start = Dates.day.shift(today, -ago);
+    var end = Dates.day.shift(today, -ago+1);
+    var filtered = subscriptions.started(start, end);
+    numbers.push(filtered.count());
+    amounts.push(filtered.mrr());
+  }
+
+  metrics.set('stripe subscriptions last week', numbers);
+  metrics.set('stripe subscription mrr last week', amounts);
+}
+
+
+/**
+ * Floor the `date` to the nearest day,
+ * while keeping in the same locale
+ * (unlike UTC'ing like Dates.day.floor).
+ */
+
+function floor (date) {
+  date = new Date(date);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  return date;
+}
